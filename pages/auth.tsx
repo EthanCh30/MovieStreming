7e5e23f8219ade0281747fa9449217660426e522
chen <565306120@qq.com>
2024-03-26 04:05:07 +1030

    Logout finish

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,15 @@
 import Input from "@/components/input";
 import axios from "axios";
 import { useCallback, useState } from "react";
+import{signIn} from 'next-auth/react';
+import { useRouter } from "next/router";
 
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 
 const Auth = () =>{
+    const router = useRouter();
 
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
@@ -15,6 +20,20 @@ const Auth = () =>{
       setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
     
+    const login = useCallback(async () => {
+      try {
+        await signIn('credentials', {
+          email,
+          password,
+          redirect: false,
+          callbackUrl: '/'
+        });
+        router.push('/');
+      } catch(error){
+        console.log(error);
+      }
+    },[email,password,router]);
+
     const register = useCallback(async () => {
       try {
         await axios.post('/api/register', {
@@ -22,10 +41,12 @@ const Auth = () =>{
           name,
           password
         });
+
+        login();
       } catch(error){
         console.log(error);
       }
-    },[email,name,password]);
+    },[email,name,password,login]);
 
 
     return (
@@ -64,9 +85,19 @@ const Auth = () =>{
             value={password}
             />
             </div>
-            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button onClick={variant === 'login' ? login : register } className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
             {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div onClick={() => signIn('google', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={32} />
+              </div>
+              <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={32} />
+              </div>
+            </div>
+
             <p className="text-neutral-500 mt-12">
             {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
             
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,36 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-
+import {getSession, signOut} from "next-auth/react"
+import { NextPageContext } from "next";
+import useCurrentUser from "@/hooks/useCurrentUser";
 const inter = Inter({ subsets: ["latin"] });
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+
 export default function Home() {
+  const {data:user } = useCurrentUser();
   return (
     <>
     <h1 className="text-2xl text-green-500">Netflix Clone</h1>
+    <p className="text-white">Logged in as : {user?.email}</p>
+    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
     </>
   );
 }
