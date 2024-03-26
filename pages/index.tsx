5995ccb3a38e645679b09c0a595b19e174ac208a
chen <565306120@qq.com>
2024-03-26 23:32:38 +1030

    Finish Billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import styles from "@/styles/Home.module.css";
 import {getSession, signOut} from "next-auth/react"
 import { NextPageContext } from "next";
 import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
+import { SiBillboard } from "react-icons/si";
+import Billboard from "@/components/Billboard";
 const inter = Inter({ subsets: ["latin"] });
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -29,9 +32,8 @@ export default function Home() {
   const {data:user } = useCurrentUser();
   return (
     <>
-    <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-    <p className="text-white">Logged in as : {user?.email}</p>
-    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+    <Navbar />
+    <Billboard />
     </>
   );
 }
