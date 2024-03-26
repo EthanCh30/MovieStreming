ebcfb5802cc166c85b4cb7479d82fe02a815802d
chen <565306120@qq.com>
2024-03-27 02:18:08 +1030

    Play button finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import { SiBillboard } from "react-icons/si";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 const inter = Inter({ subsets: ["latin"] });
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -30,10 +32,19 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const {data:user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
+  // const { data: favorites = [] } = useFavorites();
+  // const {isOpen, closeModal} = useInfoModalStore();
+
   return (
     <>
     <Navbar />
     <Billboard />
+    <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+        {/* <MovieList  /> */}
+      </div>
+   
     </>
   );
 }
