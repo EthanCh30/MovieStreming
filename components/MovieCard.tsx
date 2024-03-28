863102923f65f5c7b6788c87c0c6c434b9058080
chen <565306120@qq.com>
2024-03-28 22:55:18 +1030

    Movie Card Finish

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -87,7 +87,14 @@ return (
             New <span className='text-white'>2023</span>
         </p>
 
+        <div className="flex flex-row mt-4 gap-2 items-center"> 
+            <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+          </div>
 
+          <div className="flex flex-row items-center gap-2 mt-4 text-[8px] text-white lg:text-sm">
+            <p>{data.genre}</p>
+            
+          </div>
         </div>
       </div>
     </div>
