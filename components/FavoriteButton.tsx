7165611f72fec6ac70c7e016be0cf089279563c1
chen <565306120@qq.com>
2024-04-14 20:49:49 +0930

    format: update FavoriteButton.tsx and hello.ts

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useCallback, useMemo } from 'react';
-import { PlusIcon, CheckIcon } from '@heroicons/react/24/outline';
 import { AiOutlinePlus, AiOutlineCheck } from 'react-icons/ai';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useFavorites from '@/hooks/useFavorites';
@@ -37,13 +36,11 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
     });
     mutateFavorites();
   }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
-  
-//   const Icon = isFavorite ? CheckIcon : PlusIcon;
+
     const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
 
   return (
     <div onClick={toggleFavorites} className="cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
-      {/* <Icon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" /> */}
       <Icon className="text-white" size={25} />
     </div>
   )
